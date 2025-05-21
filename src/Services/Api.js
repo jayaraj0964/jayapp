c062b52828fbe840e2a71e50a836ea7bbd76b7ee
jayaraj0964 <jayaraj0964@gmail.com>
2025-05-21 06:09:16 +0530

    2

--- src/Services/Api.js
+++ src/Services/Api.js
@@ -9,6 +9,7 @@ export const axiosInstance = axios.create({
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type",
   },
 });
 
